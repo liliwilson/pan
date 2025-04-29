f53d836fdf61f14ab66b5d15df99bcb5cd46e1f7
Sean Cheng <seanc2841@gmail.com>
2025-04-29 12:32:32 -0400

    add one test case

--- pan/pan_test.go
+++ pan/pan_test.go
@@ -1,22 +1,64 @@
 package pan
 
 import (
+	"fmt"
 	"testing"
+	"reflect"
 
 	// "6.5840/kvraft1/rsm"
 	// "6.5840/kvsrv1/rpc"
 	// "6.5840/panapi"
 	// tester "6.5840/tester1"
+	"pan/panapi"
 )
 
-func TestCheck(t *testing.T) {
-	ts := MakeTest(t, "name", 1, 3, false, false, false, false, -1, false)
+func compareGetData(file string, expectedData string, expectedVersion panapi.Pversion, actualData string, actualVersion panapi.Pversion) (bool, string) {
+	if expectedData != actualData || expectedVersion != actualVersion {
+		return false, fmt.Sprintf("Got %s with data '%s' and version %d; expected data '%s' with version %d\n", file, actualData, actualVersion, expectedData, expectedVersion)
+	}
+	return true, ""
+}
+
+func compareGetChildren(dir string, expectedChildren []panapi.Ppath, actualChildren []panapi.Ppath) (bool, string) {
+	if !reflect.DeepEqual(expectedChildren, actualChildren) {
+		return false, fmt.Sprintf("Got %s with children %v; expected children %v\n", dir, actualChildren, expectedChildren)
+	}
+	return true, ""
+}
+
+func TestBasic(t *testing.T) {
+	ts := MakeTest(t, "Synchronous One-client Basic", 1, 3, true, false, false, false, -1, false)
 	defer ts.Cleanup()
 	ck := ts.MakeClerk()
-	for range 10 {
-		ck.Create("a/b", "hello", 0)
+	// Test Create and Exists
+	ck.Create("/a", "", panapi.Flag{})
+	ck.Create("/a/b", "hello", panapi.Flag{})
+	exists, _ := ck.Exists("/a/b", false)
+	if !exists {
+		ts.t.Fatalf("/a/b should exist\n")
 	}
-	if !ck.Exists("a/b", false) {
-		ts.t.Fatalf("bruh")
+	// Test GetData
+	data, version, _ := ck.GetData("/a/b", false)
+	if ok, err := compareGetData("/a/b", "hello", 1, data, version); !ok {
+		ts.t.Fatal(err)
+	}
+	// TestSetData
+	ck.SetData("/a/b", "bye", 1)
+	data, version, _ = ck.GetData("/a/b", false)
+	if ok, err := compareGetData("/a/b", "bye", 2, data, version); !ok {
+		ts.t.Fatal(err)
+	}
+	// Test Get Children
+	ck.Create("/a/c", "1", panapi.Flag{})
+	ck.Create("/a/d", "2", panapi.Flag{})
+	children, _ := ck.GetChildren("/a", false)
+	if ok, err := compareGetChildren("/a", []panapi.Ppath{"b", "c", "d"}, children); !ok {
+		ts.t.Fatal(err)
+	}
+	// Test Delete and Exists
+	ck.Delete("/a/b", 2)
+	exists, _ = ck.Exists("/a/b", false)
+	if exists {
+		ts.t.Fatal("/a/b should not exist\n")
 	}
 }
